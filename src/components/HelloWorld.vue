e0c03cf54b42a2fff695d8c4be648d6fb7aae03a
thanhdat <thanhdatwg@gmail.com>
2021-01-03 11:57:23 +0700

    Update Countdown Feature

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,215 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <v-row class="mt-10" style="opacity: 0.85">
+      <v-col cols="2" class="ml-10">
+        <v-row no-gutters>
+          <v-col cols="12">
+            <v-card
+              class="mx-auto"
+              max-width="344"
+              style="background-color: rgba(18 115 214 / 27%); border-color: rgba(97, 92, 92, 0.27);"
+            >
+              <v-row>
+                <v-col
+                  cols="12"
+                  class="d-flex align-end justify-center"
+                  style="padding-top: 13px"
+                >
+                  <v-icon size="3.75rem" color="white"
+                    >mdi-weather-partly-cloudy</v-icon
+                  >
+                  <div class="text-h3 ml-3 font-weight-bold white--text">
+                    {{ temp }}&deg;C
+                  </div>
+                </v-col>
+                <v-col
+                  cols="12"
+                  align="center"
+                  class="pt-0 text-subtitle-1 white--text"
+                  >{{ today }}</v-col
+                >
+                <v-col
+                  cols="12"
+                  align="center"
+                  class="pt-0 text-h4 font-weight-bold white--text"
+                  >{{ now }}</v-col
+                >
+              </v-row>
+            </v-card>
+          </v-col>
+          <v-col cols="12" class="mt-4">
+            <v-card
+              style="background-color: rgba(18 115 214 / 27%); border-color: rgba(97, 92, 92, 0.27);"
+            >
+              <v-card-text>
+                <flip-countdown deadline="2022-1-1 00:00:00"></flip-countdown>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col class="mr-10" cols="9">
+        <v-sheet height="64">
+          <v-toolbar flat>
+            <v-btn
+              outlined
+              class="mr-4"
+              color="grey darken-2"
+              @click="setToday"
+            >
+              Today
+            </v-btn>
+            <v-btn fab text small color="grey darken-2" @click="prev">
+              <v-icon small>
+                mdi-chevron-left
+              </v-icon>
+            </v-btn>
+            <v-btn fab text small color="grey darken-2" @click="next">
+              <v-icon small>
+                mdi-chevron-right
+              </v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-menu bottom right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                  <span>{{ typeToLabel[type] }}</span>
+                  <v-icon right>
+                    mdi-menu-down
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="type = 'month'">
+                  <v-list-item-title>Month</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+        </v-sheet>
+        <v-sheet height="600">
+          <v-calendar
+            ref="calendar"
+            v-model="focus"
+            color="primary"
+            :type="type"
+            @click:date="clickday($event)"
+          ></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2 d-flex justify-center">
+          Information
+        </v-card-title>
+
+        <v-card-text>
+          {{ infomationDate }}
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="primary" text @click="dialog = false">
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment";
+import FlipCountdown from "vue2-flip-countdown";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  components: { FlipCountdown },
+
+  data: () => ({
+    focus: "",
+    infomationDate: {},
+    type: "month",
+    dialog: false,
+    typeToLabel: {
+      month: "Month",
+    },
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+    weatherInterval: null,
+    today: moment().format("dddd - DD/MM/YYYY"),
+    now: moment().format("hh:mm:ss A"),
+    infoWeather: {},
+    temp: null,
+  }),
+  mounted() {
+    this.$refs.calendar.checkChange();
+    this.getInfoWeather();
+    this.weatherInterval = setInterval(() => {
+      this.getInfoWeather();
+    }, 600000);
+    setInterval(() => {
+      this.now = moment().format("hh:mm:ss A");
+      this.today = moment().format("dddd - DD/MM/YYYY");
+    }, 1000);
+  },
+  methods: {
+    getInfoWeather() {
+      axios
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=3265874a2c77ae4a04bb96236a642d2f"
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.infoWeather = response.data;
+          this.temp = Math.floor(response.data.main.temp - 273.15);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    clickday(event) {
+      this.dialog = true;
+      console.log(event, "Infomation Date");
+      this.infomationDate = { ...event };
+    },
+
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        setTimeout(() => {
+          this.selectedOpen = true;
+        }, 10);
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+  },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
